ee92dccfeb4f916ed4e09ef1e61d323df514fa17
rishikksh20 <rishikksh20@gmail.com>
2022-06-29 23:45:13 +0530

    :zap: Add Sub Band discriminator

--- modules.py
+++ modules.py
@@ -30,3 +30,59 @@ class CoMBD(torch.nn.Module):
         fmap.append(x)
         x = torch.flatten(x, 1, -1)
         return x, fmap
+
+
+class MDC(torch.nn.Module):
+
+    def __init__(self, in_channel, channel, kernel, stride, dilations, use_spectral_norm=False):
+        super(MDC, self).__init__()
+        norm_f = weight_norm if use_spectral_norm == False else spectral_norm
+        self.convs = torch.nn.ModuleList()
+        self.num_dilations = len(dilations)
+        for d in dilations:
+            self.convs.append(norm_f(Conv1d(in_channel, channel, kernel, stride=1, padding=get_padding(kernel, d),
+                                            dilation=d)))
+
+        self.conv_out = norm_f(Conv1d(channel, channel, 3, stride=stride, padding=get_padding(3, 1)))
+
+    def forward(self, x):
+        xs = None
+        for l in self.convs:
+            if xs is None:
+                xs = l(x)
+            else:
+                xs += l(x)
+
+        x = xs / self.num_dilations
+
+        x = self.conv_out(x)
+        x = F.leaky_relu(x, 0.1)
+        return x
+
+
+class SubBandDiscriminator(torch.nn.Module):
+
+    def __init__(self, channels, kernel, strides, dilations, use_spectral_norm=False):
+        super(SubBandDiscriminator, self).__init__()
+        norm_f = weight_norm if use_spectral_norm == False else spectral_norm
+
+        self.mdcs = torch.nn.ModuleList()
+        init_channel = 1
+        for c, s in zip(channels, strides):
+            self.mdcs.append(MDC(init_channel, c, kernel, s, dilations))
+            init_channel = c
+        self.conv_post = norm_f(Conv1d(c, 1, 3, padding=get_padding(3, 1)))
+
+    def forward(self, x):
+        fmap = []
+
+        for l in self.convs:
+            x = l(x)
+            fmap.append(x)
+        x = self.conv_post(x)
+        fmap.append(x)
+        x = torch.flatten(x, 1, -1)
+
+        return x, fmap
+
+
